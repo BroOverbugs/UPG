8aa394f5de4cd10802c5d54d46aadfe46b48eeab
AvazbekOlimov <olimovavazbeka@gmail.com>
2024-02-16 14:27:17 +0500

    Added ArmChair model

---
+++ Entities/Entities/Armchairs.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities;
+
+public class Armchairs : BaseEntity
+{
+    public double Price { get; set; }
+    public string TypeOfChair { get; set; } = string.Empty;
+    public string MaterialOfChair { get; set; } = string.Empty;
+    public string ColorOfChair { get; set; } = string.Empty;
+    public string ArmRestsType { get; set; } = string.Empty;
+    public string CrossMaterial { get; set; } = string.Empty;
+    public int Reclining { get; set; }
+    public string RockingMechanism { get; set; } = string.Empty;
+    public int PermissibleLoad { get; set; }
+}
---
+++ Entities/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class BaseEntity
+{
+    [Key, Required]
+    public int Id { get; set; }
+    [Required]
+    public string BrendName { get; set; } = string.Empty;
+}
