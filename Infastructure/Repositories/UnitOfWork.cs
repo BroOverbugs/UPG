3b8380f89498fd7d1c4194f68b5ef6fb3a3dfd93
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-02-21 16:13:02 +0500

    Meni modellarimni repository lari bitdi

--- Infastructure/Repositories/UnitOfWork.cs
+++ Infastructure/Repositories/UnitOfWork.cs
@@ -7,6 +7,16 @@ public class UnitOfWork(AppDBContext dBContext) : IUnitOfWork
 {
     private readonly AppDBContext _dBContext = dBContext;
 
+    public IHousingInterface Housing => new HousingRepository(_dBContext);
+
+    public IKeyboardInterface Keyboard => new KeyboardRepository(_dBContext);
+
+    public ILaptopInterface Laptop => new LaptopRepository(_dBContext);
+
+    public IMiceInterface Mice => new MiceRepository(_dBContext);
+
+    public IMonitorInterface Monitor => new MonitorRepository(_dBContext);
+
     public void Dispose()
         => GC.SuppressFinalize(this);
 
