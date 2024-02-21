bce29235fecf3a77601318bb71d20f6eb1079138
doshik <eurmonboyev@gmail.com>
2024-02-21 14:35:23 +0500

    .

--- Infastructure/Data/AppDBContext.cs
+++ Infastructure/Data/AppDBContext.cs
@@ -1,13 +1,4 @@
-﻿using Domain.Categories;
-using Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Microsoft.EntityFrameworkCore;
 namespace Infastructure.Data;
 
 public class AppDBContext : DbContext
