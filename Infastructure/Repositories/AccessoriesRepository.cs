2eee954d2a8c2669683f2790f7294487a313f525
Shoxruxmirzo <shoxruxmirzo12345@mail.ru>
2024-02-22 08:38:18 +0500

    Shoxruxmirzo

---
+++ Infastructure/Repositories/AccessoriesRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Infastructure.Data;
+using Infastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infastructure.Repositories;
+
+public class AccessoriesRepository(AppDBContext dbContext) : Repository<Accessories>(dbContext), IAccessoriesInterface
+{
+}
