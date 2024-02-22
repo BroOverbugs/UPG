c1e4cffdac687092f4ed2a2367db5775a3a97366
doshik <eurmonboyev@gmail.com>
2024-02-22 15:56:41 +0500

    update is here added 2 controllers and a lot of things

--- Application/Services/RAMService.cs
+++ Application/Services/RAMService.cs
@@ -1,6 +1,10 @@
-﻿using Application.Helpers;
+﻿ using Application.Helpers;
 using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using DTOS.Power_supplies;
 using DTOS.RAM;
+using Infastructure.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,38 +15,86 @@ namespace Application.Services;
 
 public class RAMService : IRAMService
 {
-    public Task AddCategoryAsync(AddRAMDTO ram)
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+    public RAMService(IMapper mapper, IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+    public async Task AddRAMAsync(AddRAMDTO ram)
+    {
+        var config = _mapper.Map<RAM>(ram);
+        _unitOfWork.RAM.Add(config);
+        await _unitOfWork.SaveAsync();
     }
 
-    public Task DeleteCategoryAsync(int id)
+    public async Task DeleteRAMAsync(int id)
     {
-        throw new NotImplementedException();
+        _unitOfWork.RAM.Delete(id);
+        await _unitOfWork.SaveAsync();
     }
 
-    public Task<PagedList<RAMDTO>> Filter(FilterParameters parametrs)
+    public async Task<PagedList<RAMDTO>> Filter(FilterParameters parametrs)
     {
-        throw new NotImplementedException();
+        var list = await _unitOfWork.RAM.GetAllAsync();
+
+        // Filter by title
+        if (parametrs.title is not "")
+        {
+            list = list.Where(book => book.Name.ToLower()
+                       .Contains(parametrs.Title.ToLower()));
+        }
+
+        // Filter by Price
+        list = list.Where(book => book.Price >= parametrs.minPrice &&
+                                      book.Price <= parametrs.maxPrice);
+
+        var dtos = list.Select(book => _mapper.Map<RAMDTO>(book)).ToList();
+
+        // Order by title
+        if (parametrs.orderByTitle)
+        {
+            dtos = dtos.OrderBy(book => book.Name).ToList();
+        }
+        else
+        {
+            dtos = dtos.OrderByDescending(book => book.Name).ToList();
+        }
+
+        PagedList<RAMDTO> pagedList = new(dtos, dtos.Count,
+                                                          parametrs.PageNumber, parametrs.pageSize);
+
+        return pagedList.ToPagedList(dtos, parametrs.PageSize, parametrs.PageNumber);
     }
 
-    public Task<List<RAMDTO>> GetCategoriesAsync()
+    public async Task<IEnumerable<RAMDTO>> GetRAMAsync()
     {
-        throw new NotImplementedException();
+        var config = await _unitOfWork.RAM.GetAllAsync();
+        var glaive = _mapper.Map<IEnumerable<RAMDTO>>(config);
+        return glaive;
     }
 
-    public Task<RAMDTO> GetCategoryByIdAsync(int id)
+    public async Task<RAMDTO> GetRAMByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var config = await _unitOfWork.RAM.GetByIdAsync(id);
+        return _mapper.Map<RAMDTO>(config);
     }
 
-    public Task<PagedList<RAMDTO>> GetPagetCategories(int pageSize, int pageNumber)
+    public async Task<PagedList<RAMDTO>> GetPagedRAMs(int pageSize, int pageNumber)
     {
-        throw new NotImplementedException();
+        var dtos = await GetRAMAsync();
+        PagedList<RAMDTO> pagedList = new(dtos,
+                                                          dtos.Count(),
+                                                          pageNumber,
+                                                          pageSize);
+        return pagedList.ToPagedList(dtos, pageSize, pageNumber);
     }
 
-    public Task UpdateCategoryAsync(UpdateRAMDTO ram)
+    public async Task UpdateRAMAsync(UpdateRAMDTO ram)
     {
-        throw new NotImplementedException();
+        var config = _mapper.Map<RAM>(ram);
+        _unitOfWork.RAM.Update(config);
+        await _unitOfWork.SaveAsync();
     }
 }
---
+++ Web/Controllers/RAMController.cs
@@ -0,0 +1,92 @@
+﻿using Application.Helpers;
+using Application.Interfaces;
+using DTOS.Power_supplies;
+using DTOS.RAM;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RAMController : ControllerBase
+{
+    private readonly IRAMService _ramservice;
+    public RAMController(IRAMService ramservice)
+    {
+        _ramservice = ramservice;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var categories = await _ramservice.GetRAMAsync();
+        return Ok(categories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var category = await _ramservice.GetRAMByIdAsync(id);
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(AddRAMDTO dto)
+    {
+        try
+        {
+            await _ramservice.AddRAMAsync(dto);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put(UpdateRAMDTO dto)
+    {
+        try
+        {
+            await _ramservice.UpdateRAMAsync(dto);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> Delete(int Id)
+    {
+        await _ramservice.DeleteRAMAsync(Id);
+        return Ok();
+    }
+    [HttpGet("paged")]
+    public async Task<IActionResult> GetPaged(int pageSize = 10, int pageNumber = 1)
+    {
+        var paged = await _ramservice.GetPagedRAMs(pageSize, pageNumber);
+
+        var metaData = new
+        {
+            paged.TotalCount,
+            paged.PageSize,
+            paged.CurrentPage,
+            paged.HasNext,
+            paged.HasPrevious
+        };
+
+        Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metaData));
+
+        return Ok(paged.Data);
+    }
+    [HttpGet("filter")]
+    public async Task<IActionResult> Filter([FromQuery] FilterParameters parametrs)
+    {
+        var books = await _ramservice.Filter(parametrs);
+        return Ok(books);
+    }
+}
