f7a5db91d9b0bcc5c9b309b537de838327845d38
Shoxruxmirzo <shoxruxmirzo12345@mail.ru>
2024-02-22 11:00:39 +0500

    Dtolar qoshildi

---
+++ DTOS/AccessoriesDtos/AccessoriesDto.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOS.AccessoriesDtos
+{
+    public class AccessoriesDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string BrandName { get; set; } = "";
+    }
+}
---
+++ DTOS/AccessoriesDtos/AddAccessoriesDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOS.AccessoriesDtos
+{
+    public class AddAccessoriesDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string BrandName { get; set; } = "";
+    }
+}
