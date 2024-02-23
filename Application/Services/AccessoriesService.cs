b3e84462e26d1193e3bb033193bacd74349ceb93
Shoxruxmirzo <shoxruxmirzo12345@mail.ru>
2024-02-23 08:57:26 +0500

    a.

--- Application/Services/AccessoriesService.cs
+++ Application/Services/AccessoriesService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using DTOS.AccessoriesDtos;
+using Infastructure.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,40 @@ using System.Threading.Tasks;
 
 namespace Application.Services
 {
-    public class AccessoriesService
+    public class AccessoriesService : IAccessoriesService
     {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AccessoriesService(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task AddAccessoriesAsync(AddAccessoriesDto addAccessoriesDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAccessoriesAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<AccessoriesDto>> GetAccessoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccessoriesDto> GetAccessoriesByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAccessoriesAsync(UpdateAccessoriesDto updateAccessoriesDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
