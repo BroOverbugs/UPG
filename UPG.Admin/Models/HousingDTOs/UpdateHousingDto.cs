48c0f6c5aea9d54a6840f12732b3185e1f533513
Sardorbek Saminov <sardorsaminov5@gmail.com>
2024-03-07 16:40:21 +0500

    Modellarim tugadi

--- UPG.Admin/Models/HousingDTOs/UpdateHousingDto.cs
+++ UPG.Admin/Models/HousingDTOs/UpdateHousingDto.cs
@@ -1,26 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace Domain.Entities;
+﻿namespace UPG.Admin.Models.HousingDTOs;
 
-public class Housing : BaseEntity
+public class UpdateHousingDto
 {
-    [Required] 
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string BrandName { get; set; } = "";
     public string Motherboard_form_factor { get; set; } = string.Empty;
-    [Required] 
     public string Size { get; set; } = string.Empty;
-    [Required]
     public string Maximum_cooler_height { get; set; } = string.Empty;
-    [Required]
-    public string Maximum_video_card_length { get; set; } = string.Empty; 
-    [Required]
+    public string Maximum_video_card_length { get; set; } = string.Empty;
     public string Dimensions { get; set; } = string.Empty;
-    [Required]
     public bool Built_in_fans { get; set; }
-    [Required]
     public string Spaces_for_additional_coolers { get; set; } = string.Empty;
-    [Required]
     public string Possibility_of_installing_liquid_cooling { get; set; } = string.Empty;
-    [Required]
-    public string Connectors_on_the_front_panel { get; set; } = string.Empty; 
-    [Required]
+    public string Connectors_on_the_front_panel { get; set; } = string.Empty;
     public string Case_color { get; set; } = string.Empty;
+    public List<string> ImageUrls { get; set; } = new();
 }
