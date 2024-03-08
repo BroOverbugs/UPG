edb8081fc64ce2c01a8250379fda476ad59fabb6
doshik <eurmonboyev@gmail.com>
2024-03-08 08:39:25 +0500

    .

---
+++ UPG.Admin/Models/MousePadsDTOs/UpdateMousePadsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace UPG.Admin.Models.MousePadsDTOs;
+
+public class UpdateMousePadsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string BrandName { get; set; } = "";
+    public string Material { get; set; } = string.Empty;
+    public string Dimensions { get; set; } = string.Empty;
+    public List<string> ImageUrls { get; set; } = new();
+}
---
+++ UPG.Admin/Models/PowerSuppliesDTOs/UpdatePowerSuppliesDto.cs
@@ -0,0 +1,16 @@
+﻿namespace UPG.Admin.Models.PowerSuppliesDTOs;
+
+public class UpdatePowerSuppliesDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string BrandName { get; set; } = "";
+    public string Form_factor { get; set; } = string.Empty;
+    public string Power { get; set; } = string.Empty;
+    public string Security_technologies { get; set; } = string.Empty;
+    public string Dimensions { get; set; } = string.Empty;
+    public string Certificate { get; set; } = string.Empty;
+    public List<string> ImageUrls { get; set; } = new();
+}
